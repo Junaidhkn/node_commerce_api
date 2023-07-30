a711ead81294579aaacdd02ea69e867282c36179
junaidhkn <junaidhkniazi@gmail.com>
2023-07-31 00:30:42 +0500

    api routes

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import { Request, Response } from 'express';
+import router from './router';
 
 const app = express();
 
 app.get('/', (req: Request, res: Response) => {
 	res.status(200).json({ message: 'Hello World' });
 });
+
+app.use('/api', router);
 
 app.listen(8080, () => {
 	console.log('Server is running on port localhost:8080');
