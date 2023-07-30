eee6c12259a9c0493543890b2b2f1b25c2d6df99
junaidhkn <junaidhkniazi@gmail.com>
2023-07-31 00:48:21 +0500

    middlewares

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,15 @@
 import express from 'express';
 import { Request, Response } from 'express';
 import router from './router';
+import morgan from 'morgan';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req: Request, res: Response) => {
 	res.status(200).json({ message: 'Hello World' });
