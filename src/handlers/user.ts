73e07bc8b640050c27664f2ec917294134a8d09e
junaidhkn <junaidhkniazi@gmail.com>
2023-08-02 00:22:17 +0500

    user authentication

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -1,1 +1,43 @@
 import { prisma } from '../db';
+import { comparePasswords, createJWT, hashPassword } from '../modules/auth';
+
+export const createNewUser = async (req, res) => {
+	const { username, email, password } = req.body;
+	const hashedPassword = await hashPassword(password);
+	const user = await prisma.user.create({
+		data: {
+			username,
+			email,
+			password: hashedPassword,
+		},
+	});
+
+	const token = createJWT(user);
+	res.json({ token });
+};
+
+export const loginUser = async (req, res) => {
+	const { username, password } = req.body;
+	const user = await prisma.user.findUnique({
+		where: {
+			username,
+		},
+	});
+
+	if (!user) {
+		res.status(404);
+		res.json({ message: 'User not found' });
+		return;
+	}
+
+	const valid = await comparePasswords(password, user.password);
+
+	if (!valid) {
+		res.status(401);
+		res.json({ message: 'Invalid password' });
+		return;
+	}
+
+	const token = createJWT(user);
+	res.json({ token });
+};
